7d232a79e6efa761fdef12399ab81fd1e2be3b5e
jsyeh <jsyeh@mail.mcu.edu.tw>
2021-01-28 18:57:50 +0800

    modify glutPBD for 6 variables

--- glutPBD/main.cpp
+++ glutPBD/main.cpp
@@ -1,5 +1,5 @@
-#define _NO_OLDNAMES
 #include <GL/glut.h>
+///關於 CodeBlocks 使用 freeglut 要安裝的東西
 ///freglut can be download by google: freeglut windows download
 ///https://www.transmissionzero.co.uk/software/freeglut-devel/
 ///https://www.transmissionzero.co.uk/files/software/development/GLUT/freeglut-MinGW.zip
@@ -9,6 +9,8 @@
 ///In CodeBlocks 17.12 (not 20.03, 20.03 will have problems)
 /// File-New-Project, GLUT Project, GLUT path: C:\Users\user\Desktop\freeglut
 
+///下面 _NO_OLDNAMES 這行, 是為了避開 math.h裡竟然也使用到的 y1
+#define _NO_OLDNAMES
 #include <math.h>
 #include <stdio.h>
 /// The codes are modified from Figure 14.5 in Chapter 14 of Stam's book (The art of fluid animation)
@@ -20,7 +22,6 @@
 /// 修改1: 有些 return 回傳的地方, 增加 & 變 reference 參考, 讓 lvalue 可被修改
 /// 修改2: 有些參數 加了 const, 承諾 rvalue 不會被修改
 /// 修改3: 有些地方便用 friend 讓函式可以在 class 裡被宣告
-
 
 template <int N> class dfloat
 {
@@ -190,14 +191,14 @@ public:
 
 int toy1() ///簡化的程式, 見 Stam 書 Figure 14.3 示意圖
 {
-    float x, y, z;
-    float u, v;
-    float f;
+    float x, y, z; ///variables
+    float u, v;    ///two controls
+    float f;       ///cost variable
 
-    x = y + z + u*u;
-    y = z*y + x*v;
+    x = y + z + u*u;  ///statement #1
+    y = z*y + x*v;    ///statement #2
 
-    f = x*x + y*y + u*u +v*v;
+    f = x*x + y*y + u*u +v*v;  ///cost function
     return 0;
 }
 
@@ -209,6 +210,8 @@ int toy2() ///手動完成 Automatic Differentiation 的公式, 見 Stam 書 Figure 14.4 
 
     dx[0]=dx[1]=dy[0]=dy[1]=dz[0]=dz[1]=df[0]=df[1]=0;
     du[0]=1; du[1]=0; dv[0]=0; dv[1]=1;
+    ///因為u對u微分為1, v對v微分為1 (自己對自己微分,會得到1)
+    ///這裡的陣列中, [0]都是對u微分, [1]都是對v微分
 
     x = y + z + u*u;
        dx[0] = dy[0] + dz[0] + 2*u*du[0];
@@ -217,45 +220,65 @@ int toy2() ///手動完成 Automatic Differentiation 的公式, 見 Stam 書 Figure 14.4 
     y = z*y + x*v;
        dy[0] = z*dy[0] + y*dz[0] + v*dx[0] + x*dv[0];
        dy[1] = z*dy[1] + y*dz[1] + v*dx[1] + x*dv[1];
-    f = x*x + y*y + u*u +v*v;
+    f = x*x + y*y + u*u + v*v;
        df[0] = 2*x*dx[0] + 2*y*dy[0] + 2*u*du[0] + 2*v*dv[0];
        df[1] = 2*x*dx[1] + 2*y*dy[1] + 2*u*du[1] + 2*v*dv[1];
     return 0;
 }
-float x1= 0.5, y1=0, z1=0;
-float x2=-0.5, y2=0, z2=0;
-dfloat<3> dx1, dy1, dz1;///裡面都會是0
-dfloat<3> dx2, dy2, dz2;///裡面都會是0
+///Q: 目前 + - 會收歛, 但改成 - + 就爆炸, 為什麼?
+///另外, 我把變數全部當成控制變數, 但變數值 (ex. x1, x2) 並沒有 solvePBD()中使用, 不合理
+float x1=+0.5, y1=0, z1=0;
+float x2=-0.5, y2=0, z2=0; ///總共有6個控制變數, 下面 dfloat<6>會存「對6個控制變數」微分的值
+dfloat<6> dx1, dy1, dz1;///裡面都會是0
+dfloat<6> dx2, dy2, dz2;///裡面都會是0
+///TODO: 讀 Stam's The Art of Fluid Dynamics 第14章, 了解 AD 的細節/實作意義
+///因 Stam一定是用 C/C++ 配 GLUT
 void solvePBD()
 {
+///讀Position Based Dynamics論文, 了解怎麼用 gradient 來改座標
+///https://matthias-research.github.io/pages/publications/posBasedDyn.pdf
     ///float C = abs(v1-v2) - 原始的長度
-    float dx=x1-x2, dy=y1-y2, dz=z1-z2;
-    float C = sqrt(dx*dx+dy*dy+dz*dz) - 0.8;
+    float dx=x1-x2, dy=y1-y2, dz=z1-z2; ///原始的公式中 用來算 cost function 的輔助變數
+    float C = sqrt(dx*dx+dy*dy+dz*dz) - 0.5; ///原始的cost function, 原始長度為 0.5
 
-    dx1.val(0)=x1; ///Q: 真的是這樣做嗎?
-    dx2.val(1)=x2;
+    ///細讀 Stam 第14章, 了解 (1) 控制變數 x1,y1,z1, x2,y2,z2 有6個, 所以 dfloat<6>
+    ///(2) 有些項要設1, 因為 「自己對自己微分」會得到1
+    dx1.val(0)=1; ///[0]項 是對 變數0 微分
+    dy1.val(1)=1; ///[1]項 是對 變數1 微分
+    dz1.val(2)=1; ///[2]項 是對 變數2 微分
+    dx2.val(3)=1; ///[3]項 是對 變數3 微分
+    dy2.val(4)=1; ///[4]項 是對 變數4 微分
+    dz2.val(5)=1; ///[5]項 是對 變數5 微分
     ///沒設定的,都會是0
 
-    dfloat<3> ddx=dx1-dx2, ddy=dy1-dy2, ddz=dz1-dz2;
-    dfloat<3> gC = dsqrt(ddx*ddx+ddy*ddy+ddz*ddz) - 0.8;
-    printf("%.3f %.3f %.3f\n", gC.val(0), gC.val(1), gC.val(2));
-    ///TODO: 不確定是不是對的, 要讀論文,看接下來怎麼用 gradient來改座標
-    /// https://matthias-research.github.io/pages/publications/posBasedDyn.pdf
-    ///因 Stam一定是用 C/C++ 配 GLUT
-    float len2 = ( gC.val(0)*gC.val(0)+gC.val(1)*gC.val(1)+gC.val(2)*gC.val(2) );
-    x1+=(-C/len2)*gC.val(0);
-    x2+=(-C/len2)*gC.val(1);
+    dfloat<6> ddx=dx1-dx2, ddy=dy1-dy2, ddz=dz1-dz2; ///用來算 cost function 的輔助變數
+    dfloat<6> gC = dsqrt(ddx*ddx+ddy*ddy+ddz*ddz) - 0.5;
+
+    float len2=0; ///posBasedDyn.pdf 的公式(5)
+    for(int i=0; i<6; i++){
+        ///printf(" %.3f", gC.val(i) ); ///Q: 左右的值不對稱
+        len2 += gC.val(i)*gC.val(i); ///要算出分母的平方
+    }
+    x1+=(-C/len2)*gC.val(0); ///posBasedDyn.pdf 的公式(5) 算出 delta P 回去改 P
+    x2+=(-C/len2)*gC.val(3);
+    printf(" (%.3f %.3f %.3f), (%.3f %.3f %.3f)\n", x1,y1,z1, x2,y2,z2);
+    ///長度 最後真的能保持在 自然長度 0.5
+    ///Q: 但為何整體右偏?
 }
 void display()
 {
     glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT);
-    glPushMatrix();
+    glBegin(GL_LINES); ///畫2點間的連線
+        glVertex3f(x1,y1,z1);
+        glVertex3f(x2,y2,z2);
+    glEnd();
+    glPushMatrix(); ///畫右邊的球
         glTranslatef(x1,y1,z1);
-        glutSolidSphere(0.1, 30,30);
+        glutSolidSphere(0.05, 30,30);
     glPopMatrix();
-    glPushMatrix();
+    glPushMatrix(); ///畫左邊的球
         glTranslatef(x2,y2,z2);
-        glutSolidSphere(0.1, 30,30);
+        glutSolidSphere(0.05, 30,30);
     glPopMatrix();
     glutSwapBuffers();
 }
